8e8bd8b695b1b38a8e6aaf841b4db62a15418798
MP <jasojohns@gmail.com>
2016-10-16 18:42:50 -0400

    Added new spock test case conversions

--- unitTestExample/spockTest/example/SpockDailyEventTest.groovy
+++ unitTestExample/spockTest/example/SpockDailyEventTest.groovy
@@ -1,34 +1,80 @@
 package example
 
+import java.util.List;
+import java.util.TimeZone;
+
+import org.joda.time.Days;
 import org.joda.time.Instant
 
 import spock.lang.*
 
 class SpockDailyEventTest extends SpockEventTestCase {
 
-	def jan1st2016 = getInstantAt(1, 1, 2016)
-	def dec31st2016 = getInstantAt(12, 31, 2016)
+	//2016 is good for testing leap year logic!
+	static Instant jan1st2016 = getInstantAt(1, 1, 2016)
+	static Instant dec31st2016 = getInstantAt(12, 31, 2016)
 
-	static now = Instant.now()
+	static Instant jan1st2017 = getInstantAt(1, 1, 2017)
+	static Instant dec31st2017 = getInstantAt(12, 31, 2017)
+	
+	static Instant now = Instant.now()
 
-	static filterStart = now.toDateTime().minusDays(1).toInstant()
-	static filterFifteenDaysFromNow = now.toDateTime().plusDays(15).toInstant()
-	static filterThirtyDaysFromNow = now.toDateTime().plusDays(30).toInstant()
-	static filterSixtyDaysFromNow = now.toDateTime().plusDays(60).toInstant()
-	static filterNinetyDaysFromNow = now.toDateTime().plusDays(90).toInstant()
+	static Instant filterStart = now.toDateTime().minusDays(1).toInstant()
+	static Instant filterFifteenDaysFromNow = now.toDateTime().plusDays(15).toInstant()
+	static Instant filterThirtyDaysFromNow = now.toDateTime().plusDays(30).toInstant()
+	static Instant filterSixtyDaysFromNow = now.toDateTime().plusDays(60).toInstant()
+	static Instant filterNinetyDaysFromNow = now.toDateTime().plusDays(90).toInstant()
 
-	def "test daily event over 1 year"() {
+	def "test daily event spanning a year "() {
 
+		given: "a new daily event"
+			def dailyEvent = new DailyRecurringScheduledEvent()
+				
+		when: "repeated every 1 day for a year"
+			dailyEvent.setRepeatInterval(1)
+			dailyEvent.setStartRecurrence(start)
+			dailyEvent.setEndRecurrence(end)
+
+			def instances = dailyEvent.getScheduledInstancesForTimeframe(start, end, TimeZone.getDefault())
+			
+		then: "the number of instances should match the count of the number of days in the year"
+			instances.size() == count
+		
+		where: "the start/end dates are as such"
+			start		| end			| count
+			jan1st2016  | dec31st2016 	| 366 //this is a leap year so will have one extra day!
+			jan1st2017  | dec31st2017 	| 365
+	}
+	
+	def "test daily event spanning a year and differing limits on occurences"() {
+		
 		given: "a new daily event"
 			def dailyEvent = new DailyRecurringScheduledEvent()
 
 		when: "repeated every 1 day for a year"
-			dailyEvent.setRepeatInterval(1)
 			dailyEvent.setStartRecurrence(jan1st2016)
 			dailyEvent.setEndRecurrence(dec31st2016)
 
-		then: "there are 366 instances returned for the event over the course of one full year"
-			dailyEvent.getScheduledInstancesForTimeframe(jan1st2016, dec31st2016, TimeZone.getDefault()).size() == 366
+			dailyEvent.setRepeatInterval(interval)
+			dailyEvent.setOccuranceLimit(limit != null ? new Integer(limit) : null)
+			
+			def instances = dailyEvent.getScheduledInstancesForTimeframe(jan1st2016, dec31st2016, TimeZone.getDefault())
+			
+		then: "there are differing instances returned for various values of the interval and occurrence limit"
+			instances.size() == count
+			
+		where: "these intervals and limits determine the instance count"
+			interval 	| limit	| count
+			1			| null	| 366
+			1			| 400	| 366
+			1			| 10	| 10
+			5			| 10	| 10
+			100			| 10	| 4
+			121			| 10	| 4
+			123			| 10	| 3
+			100			| 1		| 1
+			366			| null	| 1
+			367			| null	| 1
 	}
 
 	def "test daily event over several filter values"() {
@@ -55,5 +101,36 @@ class SpockDailyEventTest extends SpockEventTestCase {
 			filterFifteenDaysFromNow 	| filterThirtyDaysFromNow 	| 15
 			filterFifteenDaysFromNow	| filterSixtyDaysFromNow	| 45
 	}
+	
+	def "test 5 day interval event over several filter values"() {
+		
+		given: "a new daily event"
+			def dailyEvent = new DailyRecurringScheduledEvent()
 
+		when: "repeated every 5 day for 90 days"
+			dailyEvent.setRepeatInterval(5)
+			dailyEvent.setStartRecurrence(Instant.now())
+			dailyEvent.setEndRecurrence(Instant.now().toDateTime().plusDays(90).toInstant())
+
+			def instances = dailyEvent.getScheduledInstancesForTimeframe(start, end, TimeZone.getDefault())
+
+		then: "the number of instances should be 3 and there should be 5 days between instances"
+			instances.size() == count
+			5 == Days.daysBetween(
+					instances[0].getExecutionDatetime().toDateTime().withTimeAtStartOfDay(),
+					instances[1].getExecutionDatetime().toDateTime().withTimeAtStartOfDay()).getDays()
+			5 == Days.daysBetween(
+					instances[instances.size-2].getExecutionDatetime().toDateTime().withTimeAtStartOfDay(),
+					instances[instances.size-1].getExecutionDatetime().toDateTime().withTimeAtStartOfDay()).getDays()
+
+		where: "the filter start/end dates are as such"
+			start						| end						| count
+			filterStart 				| filterFifteenDaysFromNow 	| 3
+			filterStart 				| filterThirtyDaysFromNow 	| 6
+			filterStart 				| filterSixtyDaysFromNow 	| 12
+			filterStart 				| filterNinetyDaysFromNow 	| 18
+			filterFifteenDaysFromNow 	| filterThirtyDaysFromNow 	| 3
+			filterFifteenDaysFromNow	| filterSixtyDaysFromNow	| 9
+	}
+	
 }
--- unitTestExample/spockTest/example/SpockEventTestCase.groovy
+++ unitTestExample/spockTest/example/SpockEventTestCase.groovy
@@ -8,11 +8,11 @@ import spock.lang.*
 
 class SpockEventTestCase extends spock.lang.Specification {
 
-	protected Instant getInstantAt(int month, int day, int year) {
+	public static Instant getInstantAt(int month, int day, int year) {
 		return getInstantAt(month, day, year, DateTimeZone.forTimeZone(TimeZone.getDefault()))
 	}
 
-	protected Instant getInstantAt(int month, int day, int year, DateTimeZone zone) {
+	public static Instant getInstantAt(int month, int day, int year, DateTimeZone zone) {
 		DateTime dateTime = new DateTime(year, month, day, 12, 0, zone)
 		return dateTime.toInstant()
 	}
